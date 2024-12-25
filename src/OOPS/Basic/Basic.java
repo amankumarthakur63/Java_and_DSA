bd5eaa6600fb1c35528a652f8b37c6f799f84243
amankumarthakur63 <amankumar005562@gmail.com>
2024-12-25 23:29:49 +0530

    commit config

--- src/OOPS/Basic/Basic.java
+++ src/OOPS/Basic/Basic.java
@@ -1,2 +1,29 @@
-package OOPS.Basic;public class Basic {
+// Write a program to swapping two value using a class
+
+package OOPS.Basic;
+public class Basic {
+    public static void main(String[] args) {
+        Swap s = new Swap();
+        s.accept(3, 4);
+        s.getValue();
+        s.display();
+    }
 }
+class Swap {
+    int a, b;
+    void accept (int m, int n) {
+        a = m;
+        b = n;
+    }
+    void display () {
+        System.out.println("value of a : " + a);
+        System.out.println("value of b : " + b);
+    }
+    void getValue() {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+}
+
+
---
+++ src/OOPS/Basic/Basic2.java
@@ -0,0 +1,8 @@
+package OOPS.Basic;
+
+public class Basic2 {
+    public static void main(String[] args) {
+
+    }
+}
+
